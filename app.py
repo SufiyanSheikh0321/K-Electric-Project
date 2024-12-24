c72d66018d266bfbe0fc3f7c985f791ac4a36dcd
Muhammad Sufiyan <148213204+SufiyanSheikh0321@users.noreply.github.com>
2024-12-24 10:20:40 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+number = int(input("Enter any Current: "))
+
+if number == 1:
+    print("K_Electric on")
+    
+elif number == 0:
+    print("Ups on")  
+else:
+    print("Wrong Vale!! Only 0 , 1")
